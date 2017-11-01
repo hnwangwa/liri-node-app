7bc8fe534a8bbdfa9ee828395189e9ba0a89be77
hnwangwa <hollynwangwa@gmail.com>
2017-11-01 17:42:57 -0500

    all keys set up, need apis

---
+++ keys.js
@@ -0,0 +1,15 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'fLQ0mUyiViAkYcfDJTD57MMRZ',
+  consumer_secret: '5VPBboNMN3oY21vNdrybUXXFHfpys9xaAQUgdFaSckvxHEwFx8',
+  access_token_key: '923324294561779712-rfLBZgi7DcMwK8MFcLZkPdXfUIpyGqD',
+  access_token_secret: 'HWln2iDpBla0jTJyQmhel2IqBFjMNIPFNOd8xKF4xwxqc',
+}
+
+var spotifyKeys = {
+	client_id: '6bcef24ae7f8416a8215f446231907b1',
+	client_secret: 'e065d9642fa24b6196e6732da7a6ef42',
+}
+
+module.exports = twitterKeys, spotifyKeys;
